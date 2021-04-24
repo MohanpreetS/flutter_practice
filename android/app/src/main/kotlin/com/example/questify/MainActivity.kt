7ad68dd7af484dc4688c9a48d5dbc0809f86e32f
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-04-24 13:09:04 -0400

    first app demo

--- android/app/src/main/kotlin/com/example/questify/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.questify
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/providers/sub_task.dart
+++
@@ -1,8 +0,0 @@
-class SubTask {
-  String title;
-  String details = '';
-  Duration expectedTime = Duration.zero;
-  bool done = false;
-
-  SubTask({this.title});
-}
