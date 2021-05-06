7f9dcb0a7f5c40f786cb7d9c3f653ea1d622b955
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-05-06 13:09:04 -0400

    dice roller state

--- intro/first_app/lib/dice_roller.dart
+++ intro/first_app/lib/dice_roller.dart
@@ -12,6 +12,12 @@ class DiceRoller extends StatefulWidget {
 class _DiceRollerState extends State<DiceRoller> {
   var activeDiceImage = 'assets/images/dice-2.png';
 
+  void rollDice() {
+    setState(() {
+      activeDiceImage = 'assets/images/dice-4.png';
+    });
+  }
+
   @override
   Widget build(context) {
     return Column(
@@ -22,6 +28,19 @@ class _DiceRollerState extends State<DiceRoller> {
           width: 200,
         ),
         const SizedBox(height: 20),
+        TextButton(
+          onPressed: rollDice,
+          style: TextButton.styleFrom(
+            // padding: const EdgeInsets.only(
+            //   top: 20,
+            // ),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(
+              fontSize: 28,
+            ),
+          ),
+          child: const Text('Roll Dice'),
+        )
       ],
     );
   }
