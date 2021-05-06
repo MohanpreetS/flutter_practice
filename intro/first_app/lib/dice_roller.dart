44ec331c1c4eb4811578d2ff9c164bcd54694e68
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-05-06 13:09:04 -0400

    stateful widget basics

---
+++ intro/first_app/lib/dice_roller.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 'assets/images/dice-2.png';
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          activeDiceImage,
+          width: 200,
+        ),
+        const SizedBox(height: 20),
+      ],
+    );
+  }
+}
