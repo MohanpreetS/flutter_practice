f63e9baaf84f4ac88cf34735fd50959d434e2603
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-05-12 13:09:04 -0400

    making background for challenge excercise

--- intro/quiz_app/lib/main.dart
+++ intro/quiz_app/lib/main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
