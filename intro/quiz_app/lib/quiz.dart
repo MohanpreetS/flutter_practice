cc60c87d6297612e5888d8f950b61f6645e9077a
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-05-17 13:09:04 -0400

    ternary exp in quiz logic

--- intro/quiz_app/lib/quiz.dart
+++ intro/quiz_app/lib/quiz.dart
@@ -13,17 +13,11 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -42,7 +36,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen,
+          child: activeScreen == 'start-screen' ? StartScreen(switchScreen) : const QuestionsScreen(),
         ),
       ),
     );
