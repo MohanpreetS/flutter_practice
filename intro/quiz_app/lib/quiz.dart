0abf145ed219524c8c02de4e949b10ca2dab618e
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-05-25 13:09:04 -0400

    mapping questions to question scren

--- intro/quiz_app/lib/quiz.dart
+++ intro/quiz_app/lib/quiz.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:adv_basics/start_screen.dart';
-import 'package:adv_basics/questions_screen.dart';
+import './start_screen.dart';
+import './questions_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
