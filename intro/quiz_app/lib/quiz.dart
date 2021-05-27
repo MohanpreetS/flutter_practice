c8127e6a4a632e86efdb6cac8f6703090aa97dcf
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-05-27 13:09:04 -0400

    result screen logic

--- intro/quiz_app/lib/quiz.dart
+++ intro/quiz_app/lib/quiz.dart
@@ -46,7 +46,9 @@ class _QuizState extends State<Quiz> {
     }
 
     if (activeScreen == 'results-screen') {
-      screenWidget = const ResultsScreen();
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+      );
     }
 
     return MaterialApp(
