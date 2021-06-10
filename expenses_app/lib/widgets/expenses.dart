f68e568bfa9f59d3b26236418e410c86596161e4
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-06-10 13:09:04 -0400

    date format and icons

--- expenses_app/lib/widgets/expenses.dart
+++ expenses_app/lib/widgets/expenses.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
