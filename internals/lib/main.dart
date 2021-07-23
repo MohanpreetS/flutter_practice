fe4c606064a09532d3ef565f878bba9dfdec3d81
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-07-23 13:09:04 -0400

    main function properties

--- internals/lib/main.dart
+++ internals/lib/main.dart
@@ -4,6 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_internals/keys/keys.dart';
 
 void main() {
+  var numbers = [1, 2, 3];
+  // numbers = [4, 5, 6];
+  numbers.add(4);
+
   runApp(const App());
 }
 
