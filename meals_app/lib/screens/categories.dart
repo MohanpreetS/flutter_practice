c4d0101b9e36b062589e9478ad5fd77f8b8346c8
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-07-29 13:09:04 -0400

    categories screen setup

---
+++ meals_app/lib/screens/categories.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+    );
+  }
+}
