9d8e1433600c7c681f35f54852211bf1bfa01595
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-08-11 13:09:04 -0400

    intro to tabs

--- meals_app/lib/screens/meals.dart
+++ meals_app/lib/screens/meals.dart
@@ -7,11 +7,11 @@ import 'package:meals/widgets/meal_item.dart';
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
     super.key,
-    required this.title,
+    this.title,
     required this.meals,
   });
 
-  final String title;
+  final String? title;
   final List<Meal> meals;
 
   void selectMeal(BuildContext context, Meal meal) {
@@ -59,9 +59,13 @@ class MealsScreen extends StatelessWidget {
       );
     }
 
+    if (title == null) {
+      return content;
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text(title!),
       ),
       body: content,
     );
