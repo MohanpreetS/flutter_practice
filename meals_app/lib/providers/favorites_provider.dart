14eefdd93dbd0d81e35f7e37baa352bff6bc7363
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-08-27 13:09:04 -0400

    fixed favs proivder

--- meals_app/lib/providers/favorites_provider.dart
+++ meals_app/lib/providers/favorites_provider.dart
@@ -7,6 +7,14 @@ class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
 
   bool toggleMealFavoriteStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
+
+    if (mealIsFavorite) {
+      state = state.where((m) => m.id != meal.id).toList();
+      return false;
+    } else {
+      state = [...state, meal];
+      return true;
+    }
   }
 }
 
