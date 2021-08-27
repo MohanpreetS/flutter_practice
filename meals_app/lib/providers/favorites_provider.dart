a5b59fea84e8c632b0ffb3455e90be0e7c509e5d
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-08-27 13:09:04 -0400

    debugging favourites provider

--- meals_app/lib/providers/favorites_provider.dart
+++ meals_app/lib/providers/favorites_provider.dart
@@ -7,18 +7,9 @@ class FavoriteMealsNotifier extends StateNotifier<List<Meal>> {
 
   bool toggleMealFavoriteStatus(Meal meal) {
     final mealIsFavorite = state.contains(meal);
-
-    if (mealIsFavorite) {
-      state = state.where((m) => m.id != meal.id).toList();
-      return false;
-    } else {
-      state = [...state, meal];
-      return true;
-    }
   }
 }
 
-final favoriteMealsProvider =
-    StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
+final favoriteMealsProvider = StateNotifierProvider<FavoriteMealsNotifier, List<Meal>>((ref) {
   return FavoriteMealsNotifier();
 });
