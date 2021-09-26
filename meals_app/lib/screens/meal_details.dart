da33833c2d75ad9a789c4824e7acf4755c4a28db
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-09-26 13:09:04 -0400

    implicit animation

--- meals_app/lib/screens/meal_details.dart
+++ meals_app/lib/screens/meal_details.dart
@@ -33,7 +33,19 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween<double>(begin: 0.8, end: 1).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavorite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavorite),
+              ),
+            ),
           )
         ]),
         body: SingleChildScrollView(
