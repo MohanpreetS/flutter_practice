937cb57ef2abd119bfd6cd1b768b3ff872c6f060
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-11-13 13:09:04 -0500

    place index, detail and add screen

---
+++ fav_places/lib/screens/places.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
