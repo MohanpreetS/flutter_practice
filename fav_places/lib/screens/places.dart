da32d4d21dc4e895b32f937cb7502c4f148fc907
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-11-15 13:09:04 -0500

    designing the add place screen

--- fav_places/lib/screens/places.dart
+++ fav_places/lib/screens/places.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/screens/add_place.dart';
 import 'package:favorite_places/widgets/places_list.dart';
 import 'package:flutter/material.dart';
 
@@ -8,11 +9,17 @@ class PlacesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Your Places '),
+        title: const Text('Your Places'),
         actions: [
           IconButton(
             icon: const Icon(Icons.add),
-            onPressed: () {},
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (ctx) => const AddPlaceScreen(),
+                ),
+              );
+            },
           ),
         ],
       ),
