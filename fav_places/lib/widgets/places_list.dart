387aac4321dd47bf10b458d00f2da420fec14ae5
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-11-17 13:09:04 -0500

    place detail screen

--- fav_places/lib/widgets/places_list.dart
+++ fav_places/lib/widgets/places_list.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/screens/place_detail.dart';
 import 'package:flutter/material.dart';
 
 import 'package:favorite_places/models/place.dart';
@@ -29,6 +30,13 @@ class PlacesList extends StatelessWidget {
                 color: Theme.of(context).colorScheme.onBackground,
               ),
         ),
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (ctx) => PlaceDetailScreen(place: places[index]),
+            ),
+          );
+        },
       ),
     );
   }
