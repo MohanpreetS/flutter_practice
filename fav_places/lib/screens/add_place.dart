4baf666b3cc875c30796b1b13cfbf4da0f93d172
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-11-18 13:09:04 -0500

    using image picker

--- fav_places/lib/screens/add_place.dart
+++ fav_places/lib/screens/add_place.dart
@@ -1,3 +1,4 @@
+import 'package:favorite_places/widgets/image_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -50,6 +51,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
+            const SizedBox(height: 10),
+            ImageInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
