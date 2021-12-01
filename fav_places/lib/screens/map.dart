4a5f84a59334526d771ffaa9d04faf41a2f5d7ec
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-12-01 13:09:04 -0500

    creating the map screen widget

--- fav_places/lib/screens/map.dart
+++ fav_places/lib/screens/map.dart
@@ -22,3 +22,18 @@ class MapScreen extends StatefulWidget {
     return _MapScreenState();
   }
 }
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.isSelecting ? 'Pick your Location' : 'Your Location'), actions: [
+        if (widget.isSelecting)
+          IconButton(
+            icon: const Icon(Icons.save),
+            onPressed: () {},
+          ),
+      ]),
+    );
+  }
+}
