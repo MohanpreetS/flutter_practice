623642d5b794865ba2c91aaf5545e2101c97b34f
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-12-08 13:09:04 -0500

    creating the get db function

--- fav_places/lib/providers/user_places.dart
+++ fav_places/lib/providers/user_places.dart
@@ -9,7 +9,15 @@ import 'package:sqflite/sqlite_api.dart';
 import 'package:favorite_places/models/place.dart';
 
 Future<Database> _getDatabase() async {
-  ...
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute('CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+    },
+    version: 1,
+  );
+  return db;
 }
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
@@ -41,8 +49,7 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
     final filename = path.basename(image.path);
     final copiedImage = await image.copy('${appDir.path}/$filename');
 
-    final newPlace =
-        Place(title: title, image: copiedImage, location: location);
+    final newPlace = Place(title: title, image: copiedImage, location: location);
 
     final db = await _getDatabase();
     db.insert('user_places', {
@@ -58,7 +65,6 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
   }
 }
 
-final userPlacesProvider =
-    StateNotifierProvider<UserPlacesNotifier, List<Place>>(
+final userPlacesProvider = StateNotifierProvider<UserPlacesNotifier, List<Place>>(
   (ref) => UserPlacesNotifier(),
 );
