c5d3bc12650f8077a271e94e36fcd866154a4ba1
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2021-12-29 13:09:04 -0500

    chat bubble same sender design

--- chat_app/lib/widgets/chat_messages.dart
+++ chat_app/lib/widgets/chat_messages.dart
@@ -38,6 +38,24 @@ class ChatMessages extends StatelessWidget {
           itemBuilder: (ctx, index) {
             final chatMessage = loadedMessages[index].data();
             final nextChatMessage = index + 1 < loadedMessages.length ? loadedMessages[index + 1].data() : null;
+
+            final currentMessageUserId = chatMessage['userId'];
+            final nextMessageUserId = nextChatMessage != null ? nextChatMessage['userId'] : null;
+            final nextUserIsSame = nextMessageUserId == currentMessageUserId;
+
+            if (nextUserIsSame) {
+              return MessageBubble.next(
+                message: chatMessage['text'],
+                isMe: authenticatedUser.uid == currentMessageUserId,
+              );
+            } else {
+              return MessageBubble.first(
+                userImage: chatMessage['userImage'],
+                username: chatMessage['username'],
+                message: chatMessage['text'],
+                isMe: authenticatedUser.uid == currentMessageUserId,
+              );
+            }
           },
         );
       },
